b333aa4a4b2b8680b7ecc5d8e513fb124581b76a
714100Dayan4ik51 <dayan4ik51@gmail.com>
2023-11-11 16:09:04 +0500

    добавляем вычисление текущего маршрута

--- Taxi.py
+++ Taxi.py
@@ -5,3 +5,17 @@ def find_optimal_route(distances, tariffs):
     n = len(distances)  # количество сотрудников
     m = len(tariffs)  # количество такси
 
+    # все возможные перестановки сотрудников
+    permutations = list(itertools.permutations(range(n)))
+
+    min_cost = float('inf')  # минимальная стоимость
+    optimal_route = None
+    for route in permutations:
+        cost = 0
+
+        # Вычисляем стоимость для текущего маршрута
+        for i in range(n):
+            driver = route[i] % m
+            distance = distances[i]
+            tariff = tariffs[driver]
+            cost += distance * tariff
