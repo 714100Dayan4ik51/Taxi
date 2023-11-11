4df4c2ddbef7f07d7b34107e787601ca1d20679c
714100Dayan4ik51 <dayan4ik51@gmail.com>
2023-11-11 16:25:35 +0500

    создаем ввод данных

--- Taxi.py
+++ Taxi.py
@@ -10,6 +10,7 @@ def find_optimal_route(distances, tariffs):
 
     min_cost = float('inf')  # минимальная стоимость
     optimal_route = None
+
     for route in permutations:
         cost = 0
 
@@ -19,3 +20,14 @@ def find_optimal_route(distances, tariffs):
             distance = distances[i]
             tariff = tariffs[driver]
             cost += distance * tariff
+
+        if cost < min_cost:
+            min_cost = cost
+            optimal_route = route
+
+    return optimal_route, min_cost
+
+n = int(input("Введите кол-во сотрудников >> "))
+distances = list(map(int,input("Введите расстояние в км до дома (через пробел) >> ").split()))
+tariffs = list(map(int,input("Введите цену на такси (через пробел) >> ").split()))
+optimal_route, min_cost = find_optimal_route(distances, tariffs)
